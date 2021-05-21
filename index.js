4a69ad91447334b7a8753f7a67b155375a242cbf
Luis Garcia <jrluis21@gmail.com>
2021-05-20 19:37:35 -0500

    completed test's 6,7 and 8

--- index.js
+++ index.js
@@ -54,6 +54,7 @@ class Counter {
    */
   constructor(initialNumber) {
     // ✨ initialize whatever properties are needed
+    this.count = initialNumber
   }
 
   /**
@@ -69,7 +70,10 @@ class Counter {
    * counter.countDown() // returns 0
    */
   countDown() {
-    // ✨ implement
+    if(this.count > 0) {
+      return this.count--
+    }
+    return this.count
   }
 }
 
